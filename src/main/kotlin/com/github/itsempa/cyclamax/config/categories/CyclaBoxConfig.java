e15d9091df0cb4d19d259939827d9169a6c121ad
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-06-06 15:53:55 +0200

    Update CyclaBoxConfig.java

--- src/main/kotlin/com/github/itsempa/cyclamax/config/categories/CyclaBoxConfig.java
+++ src/main/kotlin/com/github/itsempa/cyclamax/config/categories/CyclaBoxConfig.java
@@ -8,12 +8,12 @@ import io.github.notenoughupdates.moulconfig.annotations.ConfigOption;
 public class CyclaBoxConfig {
 
     @Expose
-    @ConfigOption(name = "Cycla Highlight", desc = "cycla mobs thing highlight idk")
+    @ConfigOption(name = "Cycla Highlight", desc = "Highlight funny desert cows with a funny sphere")
     @ConfigEditorBoolean
     public boolean enabled = true;
 
     @Expose
-    @ConfigOption(name = "Sphere color", desc = "idk color of sphere")
+    @ConfigOption(name = "Sphere color", desc = "coloUr of sphere")
     @ConfigEditorColour
     public String color = "0:100:255:85:255";
 
