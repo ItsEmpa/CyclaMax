de9da26a1999540bcba41c0f843a74ef148c07d7
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-06-20 22:20:44 +0200

    refactor to use entity join world to allow for highlighting from further away
    Signed-off-by: Empa <42304516+ItsEmpa@users.noreply.github.com>

--- src/main/kotlin/com/github/itsempa/cyclamax/features/cyclabox/CyclaBox.kt
+++ src/main/kotlin/com/github/itsempa/cyclamax/features/cyclabox/CyclaBox.kt
@@ -1,55 +1,50 @@
 package com.github.itsempa.cyclamax.features.cyclabox
 
 import at.hannibal2.skyhanni.data.IslandType
-import at.hannibal2.skyhanni.data.mob.Mob
 import at.hannibal2.skyhanni.events.LorenzRenderWorldEvent
 import at.hannibal2.skyhanni.events.LorenzWorldChangeEvent
-import at.hannibal2.skyhanni.events.MobEvent
+import at.hannibal2.skyhanni.events.SecondPassedEvent
 import at.hannibal2.skyhanni.utils.ColorUtils.toChromaColor
-import at.hannibal2.skyhanni.utils.ColorUtils.withAlpha
-import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.LorenzUtils.isInIsland
-import at.hannibal2.skyhanni.utils.RenderUtils.drawColor
 import at.hannibal2.skyhanni.utils.RenderUtils.drawSphereInWorld
 import at.hannibal2.skyhanni.utils.RenderUtils.exactLocation
-import at.hannibal2.skyhanni.utils.TimeLimitedSet
-import at.hannibal2.skyhanni.utils.toLorenzVec
 import com.github.itsempa.cyclamax.CyclaMax
+import net.minecraft.entity.passive.EntityMooshroom
+import net.minecraftforge.event.entity.EntityJoinWorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 class CyclaBox {
-
     private val config get() = CyclaMax.config.cyclaBox
 
-    private val mobSet = mutableSetOf<Mob>()
+    private val entities = mutableSetOf<EntityMooshroom>()
 
     @SubscribeEvent
-    fun onMobSpawn(event: MobEvent.Spawn.SkyblockMob) {
+    fun onEntityJoinWorld(event: EntityJoinWorldEvent) {
         if (!isEnabled()) return
-        if (event.mob.name != "Farm Mooshroom") return
-        event.mob.highlight(LorenzColor.AQUA.toColor())
-        mobSet += event.mob
+        val entity = event.entity as? EntityMooshroom ?: return
+        entities += entity
     }
 
     @SubscribeEvent
-    fun onMobDeSpawn(event: MobEvent.DeSpawn.SkyblockMob) {
-        mobSet -= event.mob
+    fun onSecondPassed(event: SecondPassedEvent) {
+        if (!isEnabled()) return
+        entities.removeIf { it.isDead }
     }
 
     @SubscribeEvent
     fun onWorldChange(event: LorenzWorldChangeEvent) {
-        mobSet.clear()
+        entities.clear()
     }
 
     @SubscribeEvent
     fun onRenderWorld(event: LorenzRenderWorldEvent) {
         if (!isEnabled()) return
-        mobSet.filter { !it.baseEntity.isDead }.forEach {
-            val pos = event.exactLocation(it.baseEntity).add(y = 0.75)
+        entities.filter { !it.isDead }.forEach {
+            val pos = event.exactLocation(it).add(y = 0.75)
             event.drawSphereInWorld(
                 config.color.toChromaColor(),
                 pos,
-                1.5f
+                1.5f,
             )
         }
     }
