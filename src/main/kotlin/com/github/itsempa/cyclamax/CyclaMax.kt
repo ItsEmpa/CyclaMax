c040149de9b1928f810942704e5c38f06e85d15a
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-06-20 22:56:24 +0200

    add config autosave every 60 seconds
    Signed-off-by: Empa <42304516+ItsEmpa@users.noreply.github.com>

--- src/main/kotlin/com/github/itsempa/cyclamax/CyclaMax.kt
+++ src/main/kotlin/com/github/itsempa/cyclamax/CyclaMax.kt
@@ -1,5 +1,6 @@
 package com.github.itsempa.cyclamax
 
+import at.hannibal2.skyhanni.events.SecondPassedEvent
 import com.github.itsempa.cyclamax.commands.ConfigCommand
 import com.github.itsempa.cyclamax.config.categories.Features
 import com.github.itsempa.cyclamax.features.CyclaBox
@@ -10,6 +11,7 @@ import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Loader
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.io.File
 
 @Mod(
@@ -25,6 +27,13 @@ class CyclaMax {
         loadModule(CyclaBox())
         loadModule(VampireMask())
         ClientCommandHandler.instance.registerCommand(ConfigCommand)
+    }
+
+    @SubscribeEvent
+    fun onSecondPassed(event: SecondPassedEvent) {
+        if (event.repeatSeconds(60)) {
+            managedConfig.saveToFile()
+        }
     }
 
     private fun loadModule(obj: Any) {
